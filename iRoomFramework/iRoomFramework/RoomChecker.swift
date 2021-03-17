f093e0ac80e2b90de87e73a0ed083ac93884a7f0
ibilikstar <ibilikstarproperty@gmail.com>
2021-03-17 18:04:22 +0800

    Initial Commit

---
+++ iRoomFramework/iRoomFramework/RoomChecker.swift
@@ -0,0 +1,18 @@
+//
+//  iRoomChecker.swift
+//  iRoomFramework
+//
+//  Created by Dilip kumar on 17/03/2021.
+//
+
+import Foundation
+
+
+class RoomChecker {
+	
+	private init() {}
+	
+	public static func check() -> String {
+		return "Checking the Pod"
+	}
+}
---
+++ iRoomFramework/iRoomFramework/iRoomFramework.h
@@ -0,0 +1,18 @@
+//
+//  iRoomFramework.h
+//  iRoomFramework
+//
+//  Created by Dilip kumar on 17/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iRoomFramework.
+FOUNDATION_EXPORT double iRoomFrameworkVersionNumber;
+
+//! Project version string for iRoomFramework.
+FOUNDATION_EXPORT const unsigned char iRoomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iRoomFramework/PublicHeader.h>
+
+
